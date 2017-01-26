130a7d5ef0e35c0f00b698645ef82d5305954c0e
Gijs van Veen <gijs@splendo.com>
2017-01-26 11:01:06 +0100

    Added shared library

---
+++ shared/src/main/java/shared/app/splendo/model/MainModel.java
@@ -0,0 +1,7 @@
+package shared.app.splendo.model;
+
+/**
+ * Created by gijsvanveen on 26/01/2017.
+ */
+public class MainModel {
+}
---
+++ shared/src/main/java/shared/app/splendo/viewmodel/MainViewModel.java
@@ -0,0 +1,7 @@
+package shared.app.splendo.viewmodel;
+
+/**
+ * Created by gijsvanveen on 26/01/2017.
+ */
+public class MainViewModel {
+}
